157a24c0490553777e7c3c20fc4fd8ed0c601a6e
Hermetheus <allanmolson@gmail.com>
2020-07-13 20:50:48 -0500

    Message Controller & Maps

---
+++ DatingApp.API/Dtos/MessageForCreationDto.cs
@@ -0,0 +1,12 @@
+using System;
+namespace DatingApp.API.Dtos {
+    public class MessageForCreationDto {
+        public int SenderId { get; set; }
+        public int RecipientId { get; set; }
+        public System.DateTime MessageSent { get; set; }
+        public string Content { get; set; }
+        public MessageForCreationDto () {
+            MessageSent = DateTime.Now;
+        }
+    }
+}
